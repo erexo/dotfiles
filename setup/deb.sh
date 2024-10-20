dd85c8d57a74cc5ad74de9c722124674b0ba37d2
Kacper Drobny <kacper@arista.com>
2024-10-20 11:36:34 +0200

    improved deb

--- setup/deb.sh
+++ setup/deb.sh
@@ -1,9 +1,10 @@
 DOT_PATH="$(pwd -P)"/..
+mkdir -p ~/.config
 ln -sf "$DOT_PATH/.config/nvim" ~/.config/nvim
 ln -sf "$DOT_PATH/.zshrc" ~/.zshrc
 echo "> links created"
 
-sudo apt install -y zsh curl
+sudo apt install -y build-essential zsh curl
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc
 echo "> zsh installed"
 
@@ -13,8 +14,8 @@ rm -rf ~/go/go
 wget -qO- https://go.dev/dl/$GO_VER.linux-amd64.tar.gz | tar -xz -C ~/go
 echo "> $GO_VER installed"
 
-go install github.com/go-delve/delve/cmd/dlv@latest
-apt install -y xclip ripgrep fd-find unzip
+~/go/go/bin/go install github.com/go-delve/delve/cmd/dlv@latest
+sudo apt install -y xclip ripgrep fd-find unzip
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
 rm nvim-linux64.tar.gz
